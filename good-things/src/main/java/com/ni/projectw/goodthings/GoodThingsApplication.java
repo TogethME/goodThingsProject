d2039263a62f40ab3892c454437bb7fd6ef4dd9b
RR <inlows@163.com>
2018-07-15 00:38:25 +0800

    ---------------goodthings------------------

---
+++ good-things/src/main/java/com/ni/projectw/goodthings/GoodThingsApplication.java
@@ -0,0 +1,12 @@
+package com.ni.projectw.goodthings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoodThingsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GoodThingsApplication.class, args);
+    }
+}
